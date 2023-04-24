06d52f64aadc8b9f22876d2b56415ac7b5635879
Philipp Eitel <git@philippeitel.com>
2023-04-24 15:03:57 +0200

    Aufgabe 2: Koordinaten in Formulare eintragen

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -102,9 +102,17 @@ class MapManager {
  * A function to retrieve the current location and update the page.
  * It is called once the page has been fully loaded.
  */
-// ... your code here ...
+
+function callback(location) {
+    document.getElementById("tagging-lat").value = location.latitude;
+    document.getElementById("tagging-lon").value = location.longitude;
+}
+
+function updateLocation() {
+    LocationHelper.findLocation(callback);
+}
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
-    alert("Please change the script 'geotagging.js'");
+    updateLocation();
 });
